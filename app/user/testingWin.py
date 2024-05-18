3bdb3d810804364c9342d265b52bfe4caa92600f
Kykypy3a <oleg.metro2012@yandex.ru>
2024-05-18 12:02:16 +0900

    temp

--- app/user/testingWin.py
+++ app/user/testingWin.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QLabel, QGroupBox, QCheckBox
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QGroupBox, QCheckBox
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from components.new_widgets import ScaledPixmapLabel
@@ -6,23 +6,18 @@ from components.new_widgets import ScaledPixmapLabel
 
 class TestingWin(QWidget):
     def init_testing_ui(self):
-        text_test = self.list_tests.currentItem().text()
-        self.setWindowTitle(f'Краеведческий музей Благовещенска: Тест - {text_test}')
         wid = QWidget()
         self.setCentralWidget(wid)
-        self.db.get_test(id_test=[x[0] for x in self.filter if text_test in x[2]][0])
-        print(self.db.data)
-        self.answers = [(x[2], x[3]) for x in self.db.data]
-
-
         self.resize(600, 400)
         self.setFixedWidth(600)
         self.setWindowIcon(QIcon('resources/favicon.ico'))
+
         self.question = QLabel()
         self.image = ScaledPixmapLabel(alignment=Qt.AlignCenter)
         self.image.setStyleSheet('border: 1px solid black;')
         self.image.setScaledContents(False)
         self.image.setFixedSize(200, 200)
+
         main_l = QVBoxLayout()
         h_l1 = QHBoxLayout()
         h_l2 = QHBoxLayout()
@@ -35,13 +30,13 @@ class TestingWin(QWidget):
         main_l.addLayout(h_l1, 3)
         main_l.addLayout(h_l2)
         self.vg_l = QVBoxLayout()
-        for _ in range(1, 3):
-            self.add_answer()
         answers_group.setLayout(self.vg_l)
         main_l.addWidget(answers_group)
         main_l.addLayout(h_l4)
         main_l.addStretch()
-        self.setLayout(main_l)
+        wid.setLayout(main_l)
+
+        self.task_formation(self.list_tests.currentItem().text())
 
     def add_answer(self):
         if len(self.answers) < 8:
@@ -53,3 +48,12 @@ class TestingWin(QWidget):
             h_l.addWidget(answer)
             self.vg_l.addLayout(h_l)
             self.adjustSize()
+
+    def task_formation(self, text_test):
+        self.setWindowTitle(f'Краеведческий музей Благовещенска: Тест - {text_test}')
+        self.db.get_test(id_test=[x[0] for x in self.filter if text_test in x[2]][0])
+        print(self.db.data)
+        self.answers = [(x[2], x[3]) for x in self.db.data]
+        print(self.answers)
+        for _ in range(1, 3):
+            self.add_answer()
