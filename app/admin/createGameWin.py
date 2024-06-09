f5b5b8318406376f7ca227d0b4fd3166f1ef9889
BesedinOleg <79640118+MrKykypy3ka@users.noreply.github.com>
2024-06-09 13:15:15 +0900

    big design

--- app/admin/createGameWin.py
+++ app/admin/createGameWin.py
@@ -4,7 +4,7 @@ from components.functions import button_animation
 from components.new_widgets import ScaledPixmapLabel
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QLineEdit, QMainWindow, QComboBox,
-                             QGraphicsDropShadowEffect, QFileDialog)
+                             QGraphicsDropShadowEffect, QFileDialog, QMessageBox, QLabel)
 from PyQt5.QtGui import QIcon, QColor, QPixmap, QImage
 from PyQt5.QtCore import pyqtSignal, Qt
 from database.scripts.db import Data
@@ -17,7 +17,10 @@ class CreateGameWin(QMainWindow):
         super().__init__(parent=parent)
         self.db = Data('database/Museum.db')
         self.db.get_games_types()
+        self.puzzle_ui()
+        self.changeling_ui()
         self.init_ui()
+        self.edit_configurator()
         self.tasks = list()
 
     def init_ui(self):
@@ -25,7 +28,6 @@ class CreateGameWin(QMainWindow):
         self.setWindowIcon(QIcon('resources/favicon.ico'))
         self.resize(1280, 720)
         self.setFixedSize(1280, 720)
-
         self.win = QWidget()
         self.setCentralWidget(self.win)
         self.list_games = QComboBox()
@@ -34,74 +36,116 @@ class CreateGameWin(QMainWindow):
         self.main_l = QVBoxLayout()
         self.main_l.addStretch()
         self.main_l.addWidget(self.list_games)
-        self.win.setLayout(self.main_l)
+
 
         self.list_games.currentTextChanged.connect(self.edit_configurator)
+        self.accept_btn = QPushButton('Создать игру')
+
+        h_l3 = QHBoxLayout()
+        h_l3.addStretch(5)
+        h_l3.addWidget(self.accept_btn, 2)
+
+        self.accept_btn.setObjectName('create')
+        self.win.setObjectName('transparent')
+        self.accept_btn.clicked.connect(partial(button_animation, btn=self.accept_btn, win=self, f=self.game_formation))
+        self.accept_btn.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=5,
+                                                                    xOffset=4,
+                                                                    yOffset=4,
+                                                                    color=QColor(0, 0, 0)))
+        self.main_l.addWidget(self.puzzle_win)
+        self.main_l.addWidget(self.changeling_win)
+        self.main_l.addStretch()
+        self.main_l.addLayout(h_l3)
+
+        self.win.setLayout(self.main_l)
 
     def edit_configurator(self):
-        if self.list_games.currentData() == 'Пятнашки':
-            self.show_puzzle_ui()
+        if self.list_games.currentText() == 'Пятнашки':
+            self.puzzle_win.show()
+            self.changeling_win.hide()
+        elif self.list_games.currentText() == 'Перевёртыши':
+            self.puzzle_win.hide()
+            self.changeling_win.show()
+        else:
+            self.puzzle_win.hide()
+            self.changeling_win.hide()
 
-    def show_puzzle_ui(self):
+    def puzzle_ui(self):
+        self.puzzle_win = QWidget()
+        main_l = QVBoxLayout()
         self.title = QLineEdit()
         self.title.setPlaceholderText('Введите название игры')
-
-
         self.image = ScaledPixmapLabel(alignment=Qt.AlignCenter)
         self.image.setStyleSheet('border: 1px solid black;')
         self.image.setScaledContents(False)
         self.image.setFixedSize(200, 200)
         self.add_image_btn = QPushButton('Добавить изображение')
-        self.accept_btn = QPushButton('Создать игру')
-
         h_l1 = QHBoxLayout()
         h_l2 = QHBoxLayout()
-        h_l3 = QHBoxLayout()
-
-        self.main_l.addStretch()
-        self.main_l.addWidget(self.title)
-
+        main_l.addStretch()
+        main_l.addWidget(self.title)
         h_l1.addWidget(self.image, 2)
         h_l1.addStretch(5)
-        self.main_l.addLayout(h_l1, 3)
-
+        main_l.addLayout(h_l1, 3)
         h_l2.addWidget(self.add_image_btn, 2)
         h_l2.addStretch(5)
-        self.main_l.addLayout(h_l2)
+        main_l.addLayout(h_l2)
+        main_l.addStretch()
+        self.puzzle_win.setLayout(main_l)
 
-        h_l3.addStretch(5)
-        h_l3.addWidget(self.accept_btn, 2)
-        self.main_l.addLayout(h_l3)
-
-        self.main_l.addStretch()
-
-        self.win.setObjectName('transparent')
-        self.accept_btn.setObjectName('create')
         self.add_image_btn.setObjectName('create')
+        self.puzzle_win.setObjectName('games')
+        self.add_image_btn.clicked.connect(partial(button_animation, btn=self.add_image_btn, win=self, f=self.load_image))
+        self.add_image_btn.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=5, xOffset=4, yOffset=4, color=QColor(0, 0, 0)))
+
+    def changeling_ui(self):
+        self.changeling_win = QWidget()
+        self.changeling_win.setObjectName('games')
+        main_l = QVBoxLayout()
+        self.title = QLineEdit()
+        self.title.setPlaceholderText('Введите название игры')
+        images_l1 = QHBoxLayout()
+        images_l2 = QHBoxLayout()
+        self.images_list = []
+        for i in range(4):
+            image = ScaledPixmapLabel(alignment=Qt.AlignCenter)
+            image.setStyleSheet('border: 1px solid black;')
+            image.setScaledContents(False)
+            image.setFixedSize(200, 200)
+            self.images_list.append(image)
+            images_l1.addWidget(image)
+        for i in range(4):
+            image = ScaledPixmapLabel(alignment=Qt.AlignCenter)
+            image.setStyleSheet('border: 1px solid black;')
+            image.setScaledContents(False)
+            image.setFixedSize(200, 200)
+            self.images_list.append(image)
+            images_l2.addWidget(image)
+        self.add_image_btn = QPushButton('Добавить изображение')
+        main_l.addWidget(self.title)
+        main_l.addLayout(images_l1)
+        main_l.addLayout(images_l2)
+        main_l.addWidget(self.add_image_btn)
+        self.changeling_win.setLayout(main_l)
 
         self.add_image_btn.clicked.connect(partial(button_animation, btn=self.add_image_btn, win=self, f=self.load_image))
-        self.accept_btn.clicked.connect(partial(button_animation, btn=self.accept_btn, win=self, f=self.game_formation))
+        self.add_image_btn.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=5, xOffset=4, yOffset=4, color=QColor(0, 0, 0)))
 
-        self.add_image_btn.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=5,
-                                                                       xOffset=4,
-                                                                       yOffset=4,
-                                                                       color=QColor(0, 0, 0)))
-        self.accept_btn.setGraphicsEffect(QGraphicsDropShadowEffect(blurRadius=5,
-                                                                    xOffset=4,
-                                                                    yOffset=4,
-                                                                    color=QColor(0, 0, 0)))
-
-
-
-
-
-    def load_image(self):
-        fname = QFileDialog.getOpenFileName(self, 'Open file', '/home', "Images (*.png *.jpeg *.jpg)")
-        if fname[0]:
-            self.pixmap = QPixmap.fromImage(QImage(fname[0]))
-            self.image.setPixmap(self.pixmap)
-            with open(fname[0], 'rb') as file:
-                self.byte_image = file.read()
+    def load_image(self, count_image=8):
+        fnames, _ = QFileDialog.getOpenFileNames(self, 'Open files', '/home', "Images (*.png *.jpeg *.jpg)")
+        if len(fnames) == count_image:
+            self.pixmaps = []
+            self.byte_images = []
+            for fname in fnames:
+                pixmap = QPixmap.fromImage(QImage(fname))
+                self.pixmaps.append(pixmap)
+                with open(fname, 'rb') as file:
+                    byte_image = file.read()
+                    self.byte_images.append(byte_image)
+            for i in range(8):
+                self.images_list[i].setPixmap(self.pixmaps[i])
+        else:
+            QMessageBox.warning(self, 'Недостаточно изображений', 'Необходимо выбрать 8 картинок', buttons=QMessageBox.Ok)
 
     def game_formation(self):
         self.title.setStyleSheet('''''')
